ac13b47fff427446ba049ec11950ef8c16f3c06b
nna nna 19 <uchennaongit@gmail.com>
2021-01-13 19:26:26 +0100

    add association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :post_id
+
+  validates :body, presence: true, length: { minimum: 4, maximum: 250 }
+  validates :title, presence: true, length: { minimum: 6, maximum: 250 }
+  
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true, length: { minumum: 4, maximum: 16 }
+    validates :body, presence: true, length: { minimum: 15, maximum: 255 }
+
+    has_many :comments
+    belongs_to :user
+
 end
