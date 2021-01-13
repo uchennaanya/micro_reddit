9191e67afb98041adc8fa04fd1ba3a399415642b
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-01-13 20:39:22 +0200

    modify gemfile

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,7 @@
 class Comment < ApplicationRecord
-
   validates :body, presence: true, length: { minimum: 4, maximum: 250 }
   validates :title, presence: true, length: { minimum: 6, maximum: 250 }
-  
+
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minumum: 4, maximum: 16 }
+  validates :body, presence: true, length: { minimum: 15, maximum: 255 }
 
-    validates :title, presence: true, length: { minumum: 4, maximum: 16 }
-    validates :body, presence: true, length: { minimum: 15, maximum: 255 }
-
-    has_many :comments
-    belongs_to :user
-
+  has_many :comments
+  belongs_to :user
 end
